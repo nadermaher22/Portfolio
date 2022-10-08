e2f42e40e4a10ed480dd40ee9771e660f0d06303
nadermaher22 <82684161+nadermaher22@users.noreply.github.com>
2022-10-08 18:24:55 +0200

    Updates

---
+++ precache-manifest.2539ae604831c239ed054ce7549fd3ad.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "10821a0dd69e5d4ef922",
+    "url": "/portfolio/static/js/main.10821a0d.chunk.js"
+  },
+  {
+    "revision": "a3ff8f51a7756d386488",
+    "url": "/portfolio/static/js/1.a3ff8f51.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "a742749782c986a3653bcf6121cddea5",
+    "url": "/portfolio/static/media/crypto.a7427497.png"
+  },
+  {
+    "revision": "744ace2dfafa294319a3b01e63247a74",
+    "url": "/portfolio/static/media/Tag-Developer.744ace2d.png"
+  },
+  {
+    "revision": "a6afa8a094d07dfc432152456c8aa082",
+    "url": "/portfolio/static/media/NaderMaher.a6afa8a0.jpeg"
+  },
+  {
+    "revision": "377c194b9ee1c9ebd8b4055724279b69",
+    "url": "/portfolio/static/media/website1.377c194b.png"
+  },
+  {
+    "revision": "d490dd289ae795295d26e8347d0a0254",
+    "url": "/portfolio/static/media/website2.d490dd28.png"
+  },
+  {
+    "revision": "acf785e3ac4d993e3c4499af928c6c68",
+    "url": "/portfolio/static/media/website3.acf785e3.png"
+  },
+  {
+    "revision": "297398b29a9854dc2dfed0e32e9f3b9e",
+    "url": "/portfolio/static/media/Nairuz.297398b2.png"
+  },
+  {
+    "revision": "2e8a544f40a6424abc4ff17583de583b",
+    "url": "/portfolio/static/media/Vetor-Contato.2e8a544f.png"
+  },
+  {
+    "revision": "5c75442766ac16c08803443d82990e71",
+    "url": "/portfolio/static/media/Curriculum-Atualizado-Anderson-Dias.5c754427.pdf"
+  },
+  {
+    "revision": "80c460ecadfccce25e546b35ad02bbdb",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2ce6c9739ba69dc755c05abd2d647315.js"
+  "/portfolio/precache-manifest.2539ae604831c239ed054ce7549fd3ad.js"
 );
 
 workbox.clientsClaim();
